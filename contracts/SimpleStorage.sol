85e112e1a45840bd9dd6255717abf0498f1a315e
the-machine-learner <ritvikush@gmail.com>
2020-06-04 01:24:43 +0530

    storage contracts

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,13 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
-  uint storedData;
+  string ipfsHash;
 
-  function set(uint x) public {
-    storedData = x;
+  function set(string memory x) public {
+    ipfsHash = x;
   }
 
-  function get() public view returns (uint) {
-    return storedData;
+  function get() public view returns (string memory) {
+    return ipfsHash;
   }
 }
